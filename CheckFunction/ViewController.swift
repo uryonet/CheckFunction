17d16f215d180b456ca8566d68654ee924c0c40b
uryonet <uryonet@gmail.com>
2018-12-23 13:03:18 +0900

    Initial Commit

---
+++ CheckFunction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckFunction
+//
+//  Created by 米山諒 on 2018/12/23.
+//  Copyright © 2018 yoneyama ryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
